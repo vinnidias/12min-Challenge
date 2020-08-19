70f85fcdf96ffa08fc17853381c6663b56bed04c
vinnidias <vinicius.3991@gmail.com>
2020-08-19 16:05:55 -0300

    react native cli, navigation and axios setup

---
+++ audiobookPlayerApp/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return(
+    <View>
+      <Text>
+        Helo World!
+      </Text>
+    </View>
+  )
+}
