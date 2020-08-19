ce61ffe77282d6a662f608e672a9f5b0b14993af
vinnidias <vinicius.3991@gmail.com>
2020-08-19 19:00:00 -0300

    creating the player screen file

---
+++ audiobookPlayerApp/src/screens/AudioPlayer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native'
+
+const AudioPalyer = () => {
+	return (
+		<View>
+			<Text>
+				Audio Palyer
+			</Text>
+		</View>
+	)
+};
+
+export default AudioPalyer
