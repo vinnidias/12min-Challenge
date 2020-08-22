0c54dfc3c242bc49651d7f7dfb359c7cb48e1107
vinnidias <vinicius.3991@gmail.com>
2020-08-22 16:04:16 -0300

    indent adjustment

--- audiobookPlayerApp/src/components/TrackList.js
+++ audiobookPlayerApp/src/components/TrackList.js
@@ -3,18 +3,18 @@ import { View, Text, Image } from 'react-native'
 import BookTrack from './BookTrack'
 import { ScrollView } from 'react-native-gesture-handler'
 
-const TrackList = ({array, onPress}) => {
+const TrackList = ({ array, onPress }) => {
 	return (
-		<ScrollView style={{padding: 10}}>
-			{array.map(track => <BookTrack 
-				imgUrl={track.thumb_image_url} 
-				title={track.title} 
+		<ScrollView style={{ padding: 10 }}>
+			{array.map(track => <BookTrack
+				imgUrl={track.thumb_image_url}
+				title={track.title}
 				author={track.author}
 				songUrl={track.audio_url}
 				coverImgUrl={track.cover_image_url}
 				description={track.description}
-				onPress={(audio, coverImg, description, title, author)=> onPress(audio, coverImg, description, title, author)}
-				/>)}
+				onPress={(audio, coverImg, description, title, author) => onPress(audio, coverImg, description, title, author)}
+			/>)}
 		</ScrollView>
 	)
 };
