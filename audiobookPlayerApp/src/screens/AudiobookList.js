df5233bc2c01a91978a3bce26e4163079992a357
vinnidias <vinicius.3991@gmail.com>
2020-08-22 16:04:37 -0300

    indent adjustment

--- audiobookPlayerApp/src/screens/AudiobookList.js
+++ audiobookPlayerApp/src/screens/AudiobookList.js
@@ -3,13 +3,13 @@ import { View, Text } from 'react-native'
 import TrackList from '../components/TrackList'
 import axios from 'axios';
 
-const AudiobookList = ({navigation}) => {
-	const [list, setList] = useState({data: []})
-	useEffect(()=>{
+const AudiobookList = ({ navigation }) => {
+	const [list, setList] = useState({ data: [] })
+	useEffect(() => {
 		axios.get('https://run.mocky.io/v3/96f0177a-118f-43b8-9c99-e84e3dc3fa81')
-			.then(result => {setList(result.data)})
+			.then(result => { setList(result.data) })
 			.catch()
-	},[])
+	}, [])
 
 	return (
 		<View style={styles.listContainer}>
@@ -18,11 +18,11 @@ const AudiobookList = ({navigation}) => {
 					Sua Lista
 				</Text>
 			</View>
-			<View style={{flex: 4}}>
-				<TrackList array={list.data} onPress={(audio, coverImg,description, title, author)=>{
-					navigation.navigate('AudioPlayer', {audio, coverImg, description, title, author})
-				}}/>
-			</View>	
+			<View style={{ flex: 4 }}>
+				<TrackList array={list.data} onPress={(audio, coverImg, description, title, author) => {
+					navigation.navigate('AudioPlayer', { audio, coverImg, description, title, author })
+				}} />
+			</View>
 		</View>
 	)
 };
@@ -35,7 +35,7 @@ const styles = {
 	},
 
 	headerBox: {
-		backgroundColor: 'rgba(147, 56, 239, 0.6)',
+		backgroundColor: 'rgba(30, 44, 199, 0.6)',
 		flex: 1,
 		justifyContent: 'center'
 	},
